90c84ca9b72ac963057ba8bf8c2043c0062a8dc6
Anto <107312386+zioForas@users.noreply.github.com>
2022-07-27 17:32:14 +0100

    portfolio almost done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveBackground(event){
    for (let i= 0; i < shapes.length; ++i) {
       const isOdd = i % 2 !== 0;
       const BoolInr = isOdd ? -1 : 1 ;
-      shapes[i].style.transform = `translate(${x * BoolInr}px, ${y * oddIntager}px)`
+      shapes[i].style.transform = `translate(${x * BoolInr}px, ${y * BoolInr}px)`
 
    }
 }
